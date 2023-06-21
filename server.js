e585a832d68d64f58d7bb40d0c64d2166bbf96ea
Graham Spencer <gspencer447@yahoo.com>
2023-06-21 11:42:14 -0500

    completed all steps of the portal exercise

--- server.js
+++ server.js
@@ -1,1 +1,77 @@
-// Complete Events Exercise
+const { EventEmitter } = require("events");
+// const http = require("http");
+const { createServer } = require("http")
+const path = require("path")
+const fs = require("fs")
+
+const NewsLetter = new EventEmitter();
+
+// http.createServer
+
+const server = createServer((req, res)=>{
+    const chunks = []
+    const { url, method } = req
+
+    req.on("data", (chunk)=>{
+        chunks.push(chunk)
+        console.log(chunks);
+    })
+
+    req.on("end", ()=>{
+        if(url === "/newsletter_signup" && method === "POST"){
+            const body = JSON.parse(Buffer.concat(chunks).toString())
+            const newContact = `${body.name}, ${body.email}\n`
+            NewsLetter.emit("signup", newContact, res)
+            res.setHeader("Content-Type", "application/json")
+            res.write(
+                JSON.stringify({ message: "Signed up successfully!"})
+                )
+            res.end()
+        }
+        else if(url === "/newsletter_signup" && method === "GET"){
+            res.setHeader("Content-Type", "text/html")
+            const readStream = fs.createReadStream(
+                path.join(__dirname, "./public/index.html")
+            )
+            readStream.pipe(res)
+        }
+        else {
+            res.statusCode = 404;
+            res.setHeader("Content-Type", "application/json")
+            res.write(
+                JSON.stringify({ message: "Not a valid url"})
+                )
+                res.end();
+        }
+    })
+})
+
+NewsLetter.on("signup", (newContact, res)=>{
+    fs.appendFile(
+        path.join(__dirname, "./assets/newsletter.csv"),
+        newContact,
+        (err)=> {
+            if (err) {
+                NewsLetter.emit("error", err, res);
+                return;
+            }
+            console.log("You have been added to the newsletter");
+        }
+    )
+})
+
+NewsLetter.on("error", (err, res)=>{
+    res.statusCode(500);
+    console.log(err);
+    res.setHeader("Content-Type", "application/json")
+    res.write(
+        JSON.stringify({ message: "error adding content"})
+    )
+    res.end();
+})
+
+server.listen(3002, ()=>{
+    console.log("Server is listening...at port 3002");
+})
+
+
